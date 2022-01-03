5343caf86ca323897d40ed08544ea0860dbb21ec
Anamika_s <anamika.sawhney22@gmail.com>
2022-01-03 18:40:53 +0530

    Queue Demo

---
+++ Demo/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+public class Node
+{
+    public Node Next;
+    public object Value;
+}
+public class LinkedList
+{
+   private Node first;
+   private Node last;//This will have latest node
+   public int Count;
+
+public LinkedList()
+{
+  first = new Node();
+  last = first;
+}
+public void AddAtfirst(object data)
+{
+   Node newNode = new Node();
+   newNode.Value = data;
+   last.Next = newNode;
+   last = newNode;
+   Count++;
+}
+// public void AddInBetween(object data)
+// {
+
+// }
+public void AddAtStart(object data)
+{
+  Node newNode = new Node() { Value = data};
+  newNode.Next = first.Next;//new node will have reference of first's next reference
+  first.Next = newNode;//and now first will refer to new node
+  Count++;
+}
+public void RemoveFromStart()
+{
+   if (Count > 0)
+   {
+     first.Next = first.Next.Next;
+     Count--;
+   }
+   else
+   {
+     Console.WriteLine("No element exist in this linked list.");
+   }
+}/// <summary>
+/// Traverse from first and print all nodes value
+/// </summary>
+
+public void PrintAllNodes()
+{
+//Traverse from first
+Console.Write("first ->");
+Node curr = first;
+while (curr.Next != null)
+{
+curr = curr.Next;
+Console.Write(curr.Value);
+Console.Write("->");
+}
+Console.Write("NULL");
+}
+}
+class Program
+{
+    static void Main(string[] args)
+    {           
+        LinkedList lnklist = new LinkedList();
+        lnklist.PrintAllNodes();
+        Console.WriteLine();
+
+        lnklist.AddAtfirst(12);
+        lnklist.AddAtfirst("John");
+        lnklist.AddAtfirst("Peter");
+        lnklist.AddAtfirst(34);
+        lnklist.PrintAllNodes();
+        Console.WriteLine();
+
+        lnklist.AddAtStart(55);
+        lnklist.PrintAllNodes();
+        Console.WriteLine();
+
+        lnklist.RemoveFromStart();
+        lnklist.PrintAllNodes();
+
+        Console.ReadKey();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace QueueDemo
+{
+    class Program
+    {
+        static int [] num = new int[] { 0,0, 0, 0,0,0,0,0,0,0};
+        static int front=-1;
+        static int rear=-1;
+        static void Main(string[] args)
+        {
+
+   string ch = "y";
+    while(ch=="y")
+    {
+        Menu();
+        int choice ;
+        Console.WriteLine("Enter your choice");
+        choice = Int32.Parse(Console.ReadLine());
+        switch(choice)
+        {
+            case 1: 
+            {  Console.WriteLine("Enter number");
+              int x = Int32.Parse(Console.ReadLine());
+            Enqueue(x);
+            //    Console.WriteLine("Front points to " + front + " location");
+            //    Console.WriteLine("Rear points to " + rear + " location");
+             break;
+            }
+            case 2: {
+                    Dequeue(); 
+                    //Console.WriteLine("Top points to " + top + " location");
+                    break;
+            }
+            case 3 : Display(); break;
+
+        }
+         Console.WriteLine("Want to continue ? ");
+         ch = Console.ReadLine();
+
+    }      
+        }
+        static void Menu()
+        {
+            Console.WriteLine(" Main Menu ");
+            Console.WriteLine("1. Insert ");
+            Console.WriteLine("2. Delete ");
+            Console.WriteLine("3. Display ");
+
+        }
+        static void Enqueue(int x)
+        { 
+           if(rear == num.Length)
+            Console.WriteLine("Overflow");
+            else if(front == -1 && rear ==-1 )
+            {
+                num[++front] = x;
+                rear++;
+            }
+            else 
+                        {
+            num[++rear] = x;
+                    }
+        }
+        static void Dequeue()
+        {
+            if(front ==-1 || front == num.Length)
+            Console.WriteLine("Underflow");
+            else 
+            { 
+             num[front] =0;
+             front = front + 1;
+            }
+            }
+        static void Display()
+        {
+           for(int i = front ;i<=rear; i++)
+           Console.WriteLine(num[i]);
+        }
+    }
+}
